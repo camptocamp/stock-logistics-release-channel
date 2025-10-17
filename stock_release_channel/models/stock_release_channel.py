1a6e233e01c16e986b9c79f07625c374b884f890
Jacques-Etienne Baudoux <je@bcim.be>
2025-10-17 13:14:08 +0200

    [IMP] stock_release_channel: delivery date fallback
    Compute a delivery date when a delivery has been released without a channel.
    Apply same logic than on sales by taking all possible channels.

--- stock_release_channel/models/stock_release_channel.py
+++ stock_release_channel/models/stock_release_channel.py
@@ -956,6 +956,7 @@ class StockReleaseChannel(models.Model):
         return dt
 
     @property
+    @api.model
     def _delivery_date_steps(self):
         """Returns the steps to compute the delivery date."""
         return ["preparation", "delivery", "customer"]
