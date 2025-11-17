05edb2b380e501776b8a7c8705574a16966d0280
Jacques-Etienne Baudoux <je@bcim.be>
2025-11-17 12:57:04 +0100

    [FIX] stock_release_channel: candidate selection
    Use default sorting of channels. When there is a process_end_date, it is sorting by that date by default.

--- stock_release_channel/models/stock_picking.py
+++ stock_release_channel/models/stock_picking.py
@@ -186,11 +186,7 @@ class StockPicking(models.Model):
         log = self.env.context.get("assign_release_channel_log_stream")
         if log:
             log.write(f"Find possible channels domain: {domain}\n")
-        return (
-            self.env["stock.release.channel"]
-            .search(domain)
-            .sorted(key=lambda r: (not bool(r.partner_ids), r.sequence))
-        )
+        return self.env["stock.release.channel"].search(domain)
 
     @property
     def _release_channel_possible_candidate_domain(self):
